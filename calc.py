2e1780d9e08934dffddb19150d3352a938205442
CyorgGit <85780097+CyorgGit@users.noreply.github.com>
2025-09-09 18:37:20 -0500

    Code fixes and updates.
    Trimmed out unnecessary code and spaces, added more code to the thing that makes the "awesome.txt" file so it prints out more information.

--- calc.py
+++ calc.py
@@ -1,23 +1,13 @@
 import random
-
 digits = input("What numbers should the\nrandomly generated numbers be?\n")
 if digits.isnumeric() == False:
     print("Dude.")
 digital = int(digits)
 random_num = random.randint(-1 * digital, 1 * digital)
-
-"""
-sign = input("+, -, *, /")
-signs = int(sign)
-"""
-
 test_thing = input("Say a number.\n")
 if test_thing.isnumeric() == False:
     print("Dude.")
 other_thing = int(test_thing)
 print(str("Your number is " + str(other_thing)) + str(", plus a randomly generated number, ") + str(random_num) + str(".") + str("\nWhen added together, it makes ") + str(other_thing + random_num) + str(".") + str("\nWow, what a cool number."))
-
-""
 with open("awesome.txt", "w") as f:
-    f.write(test_thing)
-""
+    f.write(str("Chosen randomly generated numbers range:\n") + str(digits) + str("\nRandom number:\n") + str(random_num) + str("\nChosen number:\n") + str(test_thing) + str("\nNumbers added together:\n") + str(other_thing + random_num))
