723ff403a1a0de9feacd3119ea6da878f43348f8
CyorgGit <85780097+CyorgGit@users.noreply.github.com>
2025-09-11 22:01:24 -0500

    Update
    New objects, colors, features....yeah.

--- shop.py
+++ shop.py
@@ -1,13 +1,32 @@
 import random
+import os
+os.system('')
 random_num = random.randint(0, 100)
-objectlist = ["spoon", "bed", "microwave", "phone", "potato", "bag", "fully grown man", "desk", "toothbrush", "used burger", "Don't buy this omg Dude please no", "yo gurt", "broken in half plastic spork", "nothing", "funny cat pictures"]
+if random_num == 0:
+    random_num = "free!"
+elif random_num == 100:
+    random_num = 10000000000000000
+# I should probably like. Make this rely on a .txt file list instead of stuffing all the objects into the code.
+objectlist = ["spoon", "bed", "microwave", "phone", "potato", "bag", "fully grown man", "desk", "toothbrush", 
+"used burger", "yo gurt", "broken in half plastic spork", "nothing", 
+"funny cat pictures", "triangle-shaped pb&j sandwich", "Don't buy this omg Dude please no", "half-eaten chicken nugget", 
+"playing card", "unopened carton of chocolate milk", "keychain of a weird animal"]
 random_object = random.choice(objectlist)
-print(str("You can buy a " + str(random_object) + str(" for $") + str(random_num) + str(".")))
-the_choice = input("Will you?\nType y for Yes, n for No.\n")
-if the_choice == "y":
+print(str("You can buy a " + str(random_object) + str(" for \n\033[92m$") + str(random_num) + str("\033[97m.")))
+the_choice = input("\nWill you?\nType Y for Yes, N for No, L to Leave.\n")
+if the_choice.lower() == "y":
+    print("You spent your money on a", random_object + str("."))
     with open("choice.txt", "w") as f:
         f.write(str("You bought a ") + str(random_object) + (" for $" + str(random_num) + str(".")))
-else:
+elif the_choice.lower() == "n":
+    print("You chose not to purchase anything.")
     with open("choice.txt", "w") as f:
         f.write(str("You bought nothing. Wow."))
-input("\nPress enter to exit.\n")
+elif the_choice.lower() == "l":
+    print("Goodbye!")
+    with open("choice.txt", "w") as f:
+            f.write(str("You left the shop."))
+else:
+    with open("choice.txt", "w") as f:
+        f.write(str("Not sure what that even means.\nYou should probably write something that counts as a valid answer next time."))
+input("\n\033[94mPress enter to exit.\n")
