6a85385627f30e0bdecc1f5579201369b6d25b9a
CyorgGit <85780097+CyorgGit@users.noreply.github.com>
2025-09-16 17:54:01 -0500

    Scrolling rainbow list

---
+++ rainbow.py
@@ -0,0 +1,26 @@
+import random
+import os
+os.system('')
+
+red = "\033[101m  "
+orange = "\033[43m  "
+yellow = "\033[103m  "
+lime = "\033[102m  "
+cyan = "\033[104m  "
+blue = "\033[44m  "
+purple = "\033[45m  "
+black = "\033[40m  "
+pink = "\033[105m  "
+white = "\033[47m  "
+# Choose a pattern! Suggested by friends.
+rainbow = red + orange + yellow + lime + cyan + blue + purple + black
+lesbian = red + orange + white + pink + purple + black
+trans = cyan + pink + white + pink + cyan + black
+pan = pink + yellow + cyan + black
+while True:
+    print(rainbow)
+    print(str(" ") + rainbow)
+    print(str("  ") + rainbow)
+    print(str("   ") + rainbow)
+    print(str("  ") + rainbow)
+    print(str(" ") + rainbow)
