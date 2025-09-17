38836f4fca0247de02b6be7a47913346f151f86f
CyorgGit <85780097+CyorgGit@users.noreply.github.com>
2025-09-17 17:32:05 -0500

    Optimized!

--- scroller.py
+++ scroller.py
@@ -17,60 +17,20 @@ white = "\033[107m  "
 
 color1 = white
 color2 = light_gray
-
+color1_amount = 9
+color2_amount = 0
+    
 # Currently, this takes up way too much space. It's incredibly easy to optimize, but I don't feel like it right now.
 while True:
     for i in range(9):
-        print(color1 * 9 + black)
-        time.sleep(.01)
+        for i in range(9):
+            print(color1 * color1_amount + color2 * color2_amount + black)
+            time.sleep(.01)
+        color1_amount = color1_amount - 1
+        color2_amount = color2_amount + 1
     for i in range(9):
-        print(color1 * 8 + color2 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color1 * 7 + color2 * 2 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color1 * 6 + color2 * 3 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color1 * 5 + color2 * 4 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color1 * 4 + color2 * 5 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color1 * 3 + color2 * 6 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color1 * 2 + color2 * 7 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color1 * 1 + color2 * 8 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 9 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 8 + color1 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 7 + color1 * 2 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 6 + color1 * 3 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 5 + color1 * 4 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 4 + color1 * 5 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 3 + color1 * 6 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 2 + color1 * 7 + black)
-        time.sleep(.01)
-    for i in range(9):
-        print(color2 * 1 + color1 * 8 + black)
-        time.sleep(.01)
+        for i in range(9):
+            print(color2 * color2_amount + color1 * color1_amount + black)
+            time.sleep(.01)
+        color1_amount = color1_amount + 1
+        color2_amount = color2_amount - 1
