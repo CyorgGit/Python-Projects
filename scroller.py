aa3326e8b03d3a2e4ccc0de4156c1e27249df3a7
CyorgGit <85780097+CyorgGit@users.noreply.github.com>
2025-09-19 23:37:22 -0500

    New features, and background color is changeable.
    uploaded late oops

--- scroller.py
+++ scroller.py
@@ -17,6 +17,7 @@ white = "\033[107m  "
 # Change color1 and color2! A list of available colors is above.
 color1 = white
 color2 = light_gray
+color3 = black
 color1_amount = 9
 color2_amount = 0
 pattern = input("original, checkboard, or paul? ")
@@ -24,27 +25,27 @@ while True:
     if pattern == "original" or "":
         for i in range(9):
             for i in range(9):
-                print(color1 * color1_amount + color2 * color2_amount + black)
+                print(color1 * color1_amount + color2 * color2_amount + color3)
                 time.sleep(.01)
             color1_amount = color1_amount - 1
             color2_amount = color2_amount + 1
         for i in range(9):
             for i in range(9):
-                print(color2 * color2_amount + color1 * color1_amount + black)
+                print(color2 * color2_amount + color1 * color1_amount + color3)
                 time.sleep(.01)
             color1_amount = color1_amount + 1
             color2_amount = color2_amount - 1
     elif pattern == "checkerboard":
-        print((color1 + color2) * 4 + color1 + black)
+        print((color1 + color2) * 4 + color1 + color3)
         time.sleep(.01)
-        print((color2 + color1) * 4 + color2 + black)
+        print((color2 + color1) * 4 + color2 + color3)
         time.sleep(.01)
     elif pattern == "paul":
         random_number = random.randint(1,10)
         for i in range(random_number):
             random_number = random.randint(1,10)
             for i in range(random_number):
-                print(str(" ") + color1 * color1_amount + color2 * color2_amount + black)
+                print(str(" ") + color1 * color1_amount + color2 * color2_amount + color3)
                 time.sleep(.01)
             random_number = random.randint(1,10)
             color1_amount = random_number
@@ -54,7 +55,7 @@ while True:
         for i in range(random_number):
             random_number = random.randint(1,10)
             for i in range(random_number):
-                print(color2 * color2_amount + color1 * color1_amount + black)
+                print(color2 * color2_amount + color1 * color1_amount + color3)
                 time.sleep(.01)
             random_number = random.randint(1,10)
             color1_amount = random_number
