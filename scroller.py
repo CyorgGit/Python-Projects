42a44c069349e1ac9dc914fde538cba5aca8d228
CyorgGit <85780097+CyorgGit@users.noreply.github.com>
2025-09-30 17:08:42 -0500

    Updated a LOT!

--- scroller.py
+++ scroller.py
@@ -14,13 +14,15 @@ black = "\033[40m  "
 pink = "\033[105m  "
 light_gray = "\033[47m  "
 white = "\033[107m  "
+rainbow = red + orange + yellow + lime + cyan + blue + purple + black
 # Change color1 and color2! A list of available colors is above.
 color1 = white
 color2 = light_gray
 color3 = black
 color1_amount = 9
 color2_amount = 0
-pattern = input("original, checkboard, or paul? ")
+color3_amount = 0
+pattern = input("original, checkboard, paul, water, rainbow, or vertical? ")
 while True:
     if pattern == "original" or "":
         for i in range(9):
@@ -61,3 +63,54 @@ while True:
             color1_amount = random_number
             random_number = random.randint(1,10)
             color2_amount = random_number
+    elif pattern == "water":
+        for i in range(9):
+            random_number = random.randint(1,10)
+            for i in range(random_number):
+                print(str(" ") + blue * color1_amount + cyan * color2_amount + blue * color1_amount + blue)
+                time.sleep(.01)
+            random_number = random.randint(1,10)
+            color1_amount = random_number
+            random_number = random.randint(1,5)
+            color2_amount = random_number
+        random_number = random.randint(1,10)
+        random_number = random.randint(1,10)
+        for i in range(9):
+            random_number = random.randint(1,10)
+            for i in range(random_number):
+                print(str(" ") + cyan * color2_amount + blue * color1_amount + cyan * color1_amount + blue)
+                time.sleep(.01)
+            random_number = random.randint(1,7)
+            color1_amount = random_number
+            random_number = random.randint(1,5)
+            color2_amount = random_number
+    elif pattern == "rainbow":
+        print(rainbow)
+        time.sleep(.01)
+        print(str(" ") + rainbow)
+        time.sleep(.01)
+        print(str("  ") + rainbow)
+        time.sleep(.01)
+        print(str("   ") + rainbow)
+        time.sleep(.01)
+        print(str("  ") + rainbow)
+        time.sleep(.01)
+        print(str(" ") + rainbow)
+        time.sleep(.01)
+    if pattern == "vertical":
+        color1_amount = 25
+        for i in range(5):
+            for i in range(5):
+                print(color3 * color3_amount + color1 * color1_amount + color3 * color3_amount + color3)
+                time.sleep(.01)
+            color1_amount = color1_amount - 2
+            color2_amount = color2_amount + 1
+            color3_amount = color3_amount + 1
+        color1_amount = 15
+        for i in range(5):
+            for i in range(5):
+                print(color3 * color3_amount + color2 * color1_amount + color3 * color3_amount)
+                time.sleep(.01)
+            color1_amount = color1_amount + 2
+            color2_amount = color2_amount - 1
+            color3_amount = color3_amount - 1
