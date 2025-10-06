751e7adb7d4604e15d37cd22e97c29842ebcf2eb
CyorgGit <85780097+CyorgGit@users.noreply.github.com>
2025-10-06 17:16:46 -0500

    Woo! New scroller stuff!

--- scroller.py
+++ scroller.py
@@ -23,7 +23,7 @@ color1_amount = 9
 color2_amount = 0
 color3_amount = 0
 print(color1 + color1 + color1 + color3 + color1 + color1 + color1 + color3 + color1 + color1 + color1 + color3 + color1 + color1 + color1 + color3 + color1 + color3 + color3 + color3 + color1 + color3 + color3 + color3 + color1 + color1 + color1 + color3 + color1 + color1 + color1 + color3 + "\n" + color1 + color3 + color3 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color3 + color3 + color1 + color3 + color3 + color3 + color1 + color3 + color3 + color3 + color1 + color3 + color1 + color3 + "\n" + color1 + color1 + color1 + color3 + color1 + color3 + color3 + color3 + color1 + color1 + color3 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color3 + color3 + color1 + color3 + color3 + color3 + color1 + color1 + color3 + color3 + color1 + color1 + color3 + color3 + "\n" + color3 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color3 + color3 + color3 + color1 + color3 + color3 + color3 + color1 + color3 + color3 + color3 + color1 + color3 + color1 + color3 + "\n" + color1 + color1 + color1 + color3 + color1 + color1 + color1 + color3 + color1 + color3 + color1 + color3 + color1 + color1 + color1 + color3 + color1 + color1 + color1 + color3 + color1 + color1 + color1 + color3 + color1 + color1 + color1 + color3 + color1 + color3 + color1 + color3)
-pattern = input("\n\033[4mChoose one!\n\n\033[0mOriginal - Spinning thing.\nCheckerboard - Checkerboard/Chess pattern.\nCheckerboard 2.0 - Spinning checkerboard!\nPaul\nWater - Waterfall.\nRainbow - Wavy rainbow.\nVertical - Stairs, or an infinitely rotating cube.\nZig-Zag Text - Text goes from side to side.\nPortal - Original, but now with portals added!\nPortal Text - Zig-zag text, but with portals.\n\n\033[32m> \033[4m")
+pattern = input("\n\033[4mChoose one!\n\n\033[0mOriginal - Spinning thing.\nCheckerboard - Checkerboard/Chess pattern.\nCheckerboard 2.0 - Spinning checkerboard!\nPaul\nWater - Waterfall.\nRainbow - Wavy rainbow.\nVertical - Stairs, or an infinitely rotating cube.\nZig-Zag Text - Text goes from side to side.\nPortal - Original, but now with portals added!\nPortal Text - Zig-zag text, but with portals.\nStatic - Glitchy.\n\n\033[32m> \033[4m")
 print("\033[0m")
 while True:
         if pattern.lower() == "original" or "":
@@ -216,6 +216,22 @@ while True:
                     time.sleep(.01)
                 color1_amount = color1_amount + 1
                 color2_amount = color2_amount - 1
+        elif pattern.lower() == "static":
+            print("Epilepsy warning!\nTry changing the size of the window!\nResults heavily depend on the width of the terminal.")
+            time.sleep(2)
+            while True:
+                for i in range(9):
+                    print((color3 * 3 + color1 * 3) * color1_amount + color3)
+                    time.sleep(.01)
+                    color1_amount - 1
+                for i in range(9):
+                    print((color3 * 3 + color1 * 3) * color1_amount + color3)
+                    time.sleep(.01)
+                    color1_amount = color1_amount + 1
+        elif pattern.lower() == "one":
+            print(black * 4 + white + black + "\n" + black * 3 + white * 2 + black + "\n" + (black * 4 + white + black + "\n") * 2 + black * 3 + white * 3 + black)
+            time.sleep(1)
+            break
         else:
             print("Choose something that actually works!")
             time.sleep(1)
