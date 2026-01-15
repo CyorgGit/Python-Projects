edc3f698945563ee6cddbafc1394e2e4139ba47c
CyorgGit <85780097+CyorgGit@users.noreply.github.com>
2026-01-15 03:51:06 -0600

    Updated it to add AM/PM, along with bugfixes.

--- clock.py
+++ clock.py
@@ -5,33 +5,44 @@ def bye():
     os.system("cls" if os.name == "nt" else "clear")
 black = "\033[40m  "
 white = "\033[107m  "
-color = white
-a = color + color + color + black # ⬜⬜⬜⬛
-b = color + black + black + black # ⬜⬛⬛⬛
-c = black + black + color + black # ⬛⬛⬜⬛
-d = black + color + color + black # ⬛⬜⬜⬛
-e = color + color + black + black # ⬜⬜⬛⬛
-f = color + black # ⬜⬛
-g = color + color + black + black # ⬜⬜⬛⬛
-h = color + black + color + black # ⬜⬛⬜⬛
+a = white + white + white + black # ⬜⬜⬜⬛
+b = white + black + black + black # ⬜⬛⬛⬛
+c = black + black + white + black # ⬛⬛⬜⬛
+d = black + white + white + black # ⬛⬜⬜⬛
+e = white + white + black + black # ⬜⬜⬛⬛
+f = white + black # ⬜⬛
+g = white + white + black + black # ⬜⬜⬛⬛
+h = white + black + white + black # ⬜⬛⬜⬛
 i = black + black # ⬛⬛
-zerT = (a,h,h,h,a)
-oneT = (f,f,f,f,f)
-twoT = (a,c,a,b,a)
-thrT = (a,c,a,c,a)
-fouT = (h,h,a,c,c)
-fivT = (a,b,a,c,a)
-sixT = (a,b,a,h,a)
-sevT = (a,c,c,c,c)
-eigT = (a,h,a,h,a)
-ninT = (a,h,a,c,c)
-colT = (i,f,i,f,i)
-fonT = (zerT,oneT,twoT,thrT,fouT,fivT,sixT,sevT,eigT,ninT,colT)
+j = white + black + black + black + white + black # ⬜⬛⬛⬛⬜⬛
+k = white + white + black + white + white + black # ⬜⬜⬛⬜⬜⬛
+l = white + black + white + black + white + black # ⬜⬛⬜⬛⬜⬛
+m = white + white + white + white + white + black # ⬜⬜⬜⬜⬜⬛
+n = white + black + black + black + black + black # ⬜⬛⬛⬛⬛⬛
+o = black + white + white + white + black + black # ⬛⬜⬜⬜⬜⬛
+zerT = (a,h,h,h,a) # 0
+oneT = (f,f,f,f,f) # 1
+twoT = (a,c,a,b,a) # 2
+thrT = (a,c,a,c,a) # 3
+fouT = (h,h,a,c,c) # 4
+fivT = (a,b,a,c,a) # 5
+sixT = (a,b,a,h,a) # 6
+sevT = (a,c,c,c,c) # 7
+eigT = (a,h,a,h,a) # 8
+ninT = (a,h,a,c,c) # 9
+colT = (i,f,i,f,i) # 10
+aaaT = (o,j,m,j,j) # 11
+pppT = (m,j,m,n,n) # 12
+mmmT = (j,k,l,j,j) # 13
+fonT = (zerT,oneT,twoT,thrT,fouT,fivT,sixT,sevT,eigT,ninT,colT,aaaT,pppT,mmmT)
 utc = int(input("Difference between your timezone and UTC?\nEST is -5, CST is -6, MST is -7, PST is -8.\nSometimes you have to enter 0 to get the correct time, too.\n"))
 bye()
 while True:
     date = datetime.datetime.now()
-    hour = (int((f"{date.hour:02d}")) + utc)
+    hours = int(date.hour) + utc
+    if hours < 0:
+        hours = 0
+    hour = (f"{hours:02d}")
     hour_one = int(str(hour)[:1])
     hour_two = int(str(hour)[1:])
     minute = (f"{date.minute:02d}")
@@ -43,6 +54,13 @@ while True:
     colon = 10
     for i in range(5):
         print(''.join((fonT[hour_one])[i]) + ''.join((fonT[hour_two])[i]) + ''.join((fonT[colon])[i]) + ''.join((fonT[minute_one])[i]) + ''.join((fonT[minute_two])[i]) + ''.join((fonT[colon])[i]) + ''.join((fonT[second_one])[i]) + ''.join((fonT[second_two])[i]))
+    print("\n")
+    if hours > 12:
+        for i in range(5):
+            print(''.join((fonT[12])[i]) + ''.join((fonT[13])[i]))
+    else:
+        for i in range(5):
+            print(''.join((fonT[11])[i]) + ''.join((fonT[13])[i]))
     print("\n")
     time.sleep(1)
     bye()
